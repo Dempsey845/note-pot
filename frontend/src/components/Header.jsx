5e6b32268f758948e3d59232c6916f19c4775b41
Dempsey845 <dempseynewton1@gmail.com>
2025-05-31 11:57:02 +0100

    added dynamic scaling to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,36 +1,102 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CloseIcon } from "../icons/CloseIcon";
 import { MenuIcon } from "../icons/MenuIcon";
 import { SearchIcon } from "../icons/SearchIcon";
+import { RefreshIcon } from "../icons/RefreshIcon";
+import { ArrowBackIcon } from "../icons/ArrowBackIcon";
+import { SettingsIcon } from "../icons/SettingsIcon";
 
 function Header() {
   const [searchFocused, setSearchFocused] = useState(false);
 
+  const [size, setSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  const [showCollapsedSearchBar, setShowCollapsedSearchBar] = useState(false);
+
+  const mdWidth = 768;
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > mdWidth) {
+        setShowCollapsedSearchBar(false);
+      }
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <header>
       <div className="flex w-full border-b border-gray-400 justify-between p-2">
-        <div className="flex items-center gap-2">
-          <div className="header-hover">
-            <MenuIcon width="2em" height="2em" />
-          </div>
-          <h1 className="title">NotePot</h1>
+        <div className="flex w-full gap-20">
+          {!showCollapsedSearchBar && (
+            <div className="flex items-center gap-2 px-2">
+              <div className="header-hover">
+                <MenuIcon width="2em" height="2em" />
+              </div>
+              <h1 className="title">NotePot</h1>
+            </div>
+          )}
+          {(size.width > mdWidth || showCollapsedSearchBar) && (
+            <div
+              className={`search-bar flex items-center gap-3 px-3 py-0.5 w-1/2 ${
+                showCollapsedSearchBar && "w-full"
+              } bg-gray-200 rounded-xl cursor-text ${
+                searchFocused && "shadow-2xl bg-slate-100"
+              }`}
+            >
+              {showCollapsedSearchBar ? (
+                <div
+                  onClick={() => setShowCollapsedSearchBar(false)}
+                  className="header-hover-2"
+                >
+                  <ArrowBackIcon height="1.5em" width="1.5em" />{" "}
+                </div>
+              ) : (
+                <div className="header-hover-2">
+                  <SearchIcon height="1.5em" width="1.5em" />{" "}
+                </div>
+              )}
+              <input
+                onFocus={() => setSearchFocused(true)}
+                onBlur={() => setSearchFocused(false)}
+                placeholder={"Search"}
+                className="w-full"
+              />
+              <div className="header-hover-2">
+                <CloseIcon height="1.5em" width="1.5em" />{" "}
+              </div>
+            </div>
+          )}
         </div>
-        <div
-          className={`search-bar flex items-center gap-3 px-3 py-0.5 w-1/2 bg-gray-200 rounded-xl cursor-text ${
-            searchFocused && "shadow-2xl bg-slate-100"
-          }`}
-        >
-          <div className="header-hover-2">
-            <SearchIcon height="1.5em" width="1.5em" />{" "}
+        <div className="flex items-center">
+          {size.width <= mdWidth && (
+            <div
+              onClick={() => setShowCollapsedSearchBar(true)}
+              className="header-hover p-3"
+            >
+              <SearchIcon width="1.5em" height="1.5em" />
+            </div>
+          )}
+          <div className="header-hover p-3">
+            <RefreshIcon width="1.5em" height="1.5em" />
           </div>
-          <input
-            onFocus={() => setSearchFocused(true)}
-            onBlur={() => setSearchFocused(false)}
-            placeholder={"Search"}
-            className="w-full"
-          />
-          <div className="header-hover-2">
-            <CloseIcon height="1.5em" width="1.5em" />{" "}
+          <div className="header-hover p-3">
+            <SettingsIcon width="1.5em" height="1.5em" />
+          </div>
+
+          <div className="header-hover">
+            <div className="rounded-full w-8 h-8 overflow-hidden">
+              <img src="https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg" />
+            </div>
           </div>
         </div>
       </div>
