628f3f8c16f5da9eafd30944511c41c0086d640f
Dempsey845 <dempseynewton1@gmail.com>
2025-05-31 14:30:49 +0100

    Added NoteInput for adding notes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import NoteInput from "./components/NoteInput";
 
 function App() {
   const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
@@ -10,10 +11,15 @@ function App() {
   return (
     <div className="note-pot h-lvh">
       <Header setSidebarCollapsed={toggleSbCollapsed} />
-      <Sidebar
-        collapsed={sidebarCollapsed}
-        setCollapsed={setSidebarCollapsed}
-      />
+      <div className="flex">
+        <Sidebar
+          collapsed={sidebarCollapsed}
+          setCollapsed={setSidebarCollapsed}
+        />
+        <div className="notes flex justify-center w-full h-lvh px-20 py-5">
+          <NoteInput />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/NoteInput.jsx
@@ -0,0 +1,68 @@
+import { use, useEffect, useState } from "react";
+import { FormatIcon } from "../icons/FormatIcon";
+import { ArchiveIcon } from "../icons/ArchiveIcon";
+
+function NoteInput() {
+  const [collapsed, setCollapsed] = useState(true);
+  const [canFormat, setCanFormat] = useState(true);
+
+  const [text, setText] = useState("");
+  const [title, setTitle] = useState("");
+
+  const handleInput = (e) => {
+    e.target.style.height = "auto";
+    e.target.style.height = `${e.target.scrollHeight}px`;
+    setText(e.target.value);
+  };
+
+  const handleTitleInput = (e) => {
+    setTitle(e.target.value);
+  };
+
+  return (
+    <div className="w-128 flex flex-col h-fit bg-white p-1 border border-gray-200 rounded-xl shadow-xl">
+      {!collapsed && (
+        <input
+          onInput={handleTitleInput}
+          onFocus={() => setCanFormat(false)}
+          onBlur={() => setCanFormat(true)}
+          type="text"
+          placeholder="Title"
+          value={title}
+          className="placeholder:text-gray-600 placeholder:text-xl text-xl"
+        />
+      )}
+      <textarea
+        onInput={handleInput}
+        onSelect={() => setCollapsed(false)}
+        type="text-area"
+        placeholder="Take a note..."
+        value={text}
+        className=""
+      />
+      {!collapsed && (
+        <div className="hotbar flex justify-between px-2">
+          <div className="flex">
+            <button
+              disabled={!canFormat}
+              className={`p-2 ${canFormat ? "hover" : "button-disabled"}`}
+            >
+              <FormatIcon width="1em" height="1em" />
+            </button>
+            <button className={`p-2 hover`}>
+              <ArchiveIcon width="1.2em" height="1.2em" />
+            </button>
+          </div>
+          <button
+            onClick={() => setCollapsed(true)}
+            className={`py-1 px-5 mx-5 hover-square`}
+          >
+            Close
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default NoteInput;
